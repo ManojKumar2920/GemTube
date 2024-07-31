a2e2fdf3e3642d3ce80b1fd0c7c33c887b60481b
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-08-01 00:33:36 +0530

    gemtext page updation

--- app/dashboard/gemText/page.jsx
+++ app/dashboard/gemText/page.jsx
@@ -1,8 +1,14 @@
+import DashboardLayout from '@/components/Dashboard/DashboardLayout';
+import ProtectedRoute from '@/components/ProtectedRoutes/ProtectedRoutes';
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <ProtectedRoute>
+        <DashboardLayout>
+            <p>Gem Text</p>
+        </DashboardLayout>
+    </ProtectedRoute>
   )
 }
 
